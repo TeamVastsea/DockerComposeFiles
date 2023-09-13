5ccc4b9e16aa05c10b21f1af91e7f8c3bc517b00
Snowball_233 <wangrb.qq@outlook.com>
2023-09-14 00:43:29 +0800

    feat: add third-party server suppor

---
+++ minecraft-third-party/minecraft-server-launch-shell/launch-build.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+echo ----------------------------------------
+echo  [1.20.1-Vanilla] xkballServer
+echo  By:Snowball_233
+echo ----------------------------------------
+
+while true
+do
+
+echo Loading server...
+
+cd "/home/[30001]xkball/"
+
+python3 -m mcdreforged
+
+sleep 5
+
+done
